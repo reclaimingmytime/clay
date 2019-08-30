6c589dbcb4c7d416877d25099c68d30d1c6b2f40
reclaimingmytime <creator@hotmail.de>
2019-08-31 01:16:21 +0200

    Add files

---
+++ clay.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Clay
+
+main = putCss $
+  do
+    -- Basic elements
+    star ?
+      do  sym margin (px 0) --todo remove unit 
+          sym padding (px 0)
+    body ?
+      do  fontFamily ["Trebuchet MS", "Helvetica"] [sansSerif]
+          fontSize (px 16) 
+          sym margin (px 8)
+          background white 
+          color black 
+    iframe ?  
+      do width (pct 100)
+         height (px 730) 
+    h1 ?
+      do  fontSize inherit
+          display inline
+          fontWeight normal
+
+    -- Navigation
+    a ?
+      do  color "#0000cd" --mediumBlue
+          textDecoration none
+    ul ? 
+      do  listStyleType none
+          display inline
+          li ?
+            do display inline 
+    nav ?
+      do  display inline 
+          li ? 
+            do marginLeft (px 15) 
+
+    -- Custom classes
+    ".fullCalendar" ?
+      do fontStyle italic
+         color "#555" --grey
+    ".currentWeek" ?      
+      fontWeight bold
